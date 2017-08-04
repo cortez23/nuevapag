5742b91d371f77836c61adde1ea330c194ed31fd
cortez23 <alarcon.cortez2@gmail.com>
2017-08-04 00:21:10 -0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/reset.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div id="carousel-id" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item">
+					<img class="img-slider" data-src="img/slider/img01.jpg" alt="First slide" src="img/slider/img01.jpg">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1></h1>
+							<p></p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<img class="img-slider" data-src="img/slider/img01.jpg" alt="First slide" src="img/slider/img01.jpg">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1></h1>
+							<p></p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="item active">
+					<img class="img-slider" data-src="img/slider/img01.jpg" alt="First slide" src="img/slider/img01.jpg">
+					<div class="container">
+						<div class="carousel-caption">
+							<h1></h1>
+							<p></p>
+							<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+<!-- nav -->
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+						<li><a href="#">Link</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('header.php') ?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="caja col-lg-12">
+			<figure class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ite">
+				<a href="#">
+					<img src="img/nose/tazon.jpg" alt="Photo by Ryan Holloway on Unsplash">				
+				</a>
+			</figure>
+			<figure class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ite">
+				<a href="#">
+					<img src="img/nose/ejem.jpg" alt="img">				
+				</a>
+			</figure>
+			<figure class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ite">
+				<a href="#">
+					<img src="img/nose/ejem.jpg" alt="img">				
+				</a>
+			</figure>
+		</div>
+	</div>	
+</div>
+<?php include('footer.php') ?>
+
+		
